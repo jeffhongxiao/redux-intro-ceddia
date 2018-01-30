8a68fef329911a3553262c26dec33b1089383733
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2018-01-30 14:55:31 -0400

    extract reducer

--- src/index.js
+++ src/index.js
@@ -6,24 +6,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 
-const initialState = {
-  count: 0
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1
-      };
-    case 'DECREMENT':
-      return {
-        count: state.count - 1
-      };
-    default:
-      return state;
-  }
-}
+import reducer from './Reducer';
 
 const logger = createLogger({
     collapsed: true,
