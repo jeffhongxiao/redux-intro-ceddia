b47b5f0a5a53b553a52f8573246b4df52eae5dd0
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2018-02-02 10:03:28 -0400

    use computed property name syntax: [key]

--- src/Reducer.js
+++ src/Reducer.js
@@ -12,16 +12,12 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case ActionTypes.INCREMENT:
-      // return {
-      //   //...state, ['count'][action.data]: state['count'][action.data] + 1
-      //   ...state, count: {...state['count'], count[action.data]: {...state['count'][action.data]}}
-      // };
-      const delta = {counter1: state['counter1'] + 1};
+      const key = action.data;
+      const delta = {[key]: state[key] + 1};
       var obj = Object.assign({}, state, delta);
       return obj;
     case ActionTypes.DECREMENT:
       return {
-        //...state, ['count'][action.data]: state['count'][action.data] - 1
         ...state
       };
     default:
